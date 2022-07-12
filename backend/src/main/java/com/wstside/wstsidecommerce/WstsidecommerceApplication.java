fb1d471a5f647b070f189c7f535ded531c508ac3
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-07-11 23:34:39 -0300

    adicionando os projetos iniciais de frontend e backend

---
+++ backend/src/main/java/com/wstside/wstsidecommerce/WstsidecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.wstside.wstsidecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WstsidecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WstsidecommerceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wstside/wstsidecommerce/WstsidecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wstside.wstsidecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WstsidecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
